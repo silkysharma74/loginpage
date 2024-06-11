888b8081b656e8cabb93aef3b227f3e20070e870
Silky Sharma <121107166+silkysharma74@users.noreply.github.com>
2024-06-11 16:32:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Login from './Login';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
+
